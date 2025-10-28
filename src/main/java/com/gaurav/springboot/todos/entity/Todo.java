ee8f281cd6c6545e062d5fd335f761d0e4b3e04e
Gaurav <gauravwadibhasme0850@gmail.com>
2025-10-28 21:32:56 +0530

    Todo entity

---
+++ src/main/java/com/gaurav/springboot/todos/entity/Todo.java
@@ -0,0 +1,80 @@
+package com.gaurav.springboot.todos.entity;
+
+import jakarta.persistence.*;
+
+@Table(name = "todos")
+@Entity
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private int priority;
+
+    @Column(nullable = false)
+    private boolean complete;
+
+
+    //    private User owner;
+
+    //    Default constructor (required by JPA)
+    public Todo() {
+    }
+
+    public Todo(long id, String title, String description, int priority, boolean complete) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.complete = complete;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isComplete() {
+        return complete;
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
+}
