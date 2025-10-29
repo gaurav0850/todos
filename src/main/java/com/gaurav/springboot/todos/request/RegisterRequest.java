db347ccf1dd1195e16744112a79dc24fa9cdec75
gaurav0850 <gauravwadibhasme0850@gmail.com>
2025-10-29 16:14:02 +0530

    RegisterRequest to validate fields

---
+++ src/main/java/com/gaurav/springboot/todos/request/RegisterRequest.java
@@ -0,0 +1,63 @@
+package com.gaurav.springboot.todos.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public class RegisterRequest {
+
+    @NotEmpty(message = "First name is mandatory")
+    @Size(min = 3, max = 30, message = "First name length must be between 3-30")
+    private String firstName;
+
+    @NotEmpty(message = "Last name is mandatory")
+    @Size(min = 3, max = 30, message = "Last name length must be between 3-30")
+    private String lastName;
+
+    @NotEmpty(message = "Email is mandatory")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotEmpty(message = "Password is mandatory")
+    @Size(min = 5, max = 30, message = "Last name length must be between 5-30")
+    private String password;
+
+    public RegisterRequest(String email, String firstName, String lastName, String password) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
