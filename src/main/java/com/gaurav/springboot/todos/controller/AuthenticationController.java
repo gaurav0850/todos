ea6f9c99b71d91647c603231ba36906d55cdf7f9
Gaurav <gauravwadibhasme0850@gmail.com>
2025-11-01 10:12:57 +0530

    login controller

--- src/main/java/com/gaurav/springboot/todos/controller/AuthenticationController.java
+++ src/main/java/com/gaurav/springboot/todos/controller/AuthenticationController.java
@@ -1,14 +1,14 @@
 package com.gaurav.springboot.todos.controller;
 
+import com.gaurav.springboot.todos.request.AuthenticationRequest;
 import com.gaurav.springboot.todos.request.RegisterRequest;
+import com.gaurav.springboot.todos.response.AuthenticationResponse;
 import com.gaurav.springboot.todos.service.AuthenticationService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -25,6 +25,13 @@ public class AuthenticationController {
     @PostMapping("/register")
     public void register(@Valid @RequestBody RegisterRequest registerRequest) throws Exception {
         authenticationService.register(registerRequest);
+    }
+
+    @Operation(summary = "Login a user", description = "Submit email & password to authenticate user")
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/login")
+    public AuthenticationResponse login(@Valid @RequestBody AuthenticationRequest authenticationRequest) {
+        return authenticationService.login(authenticationRequest);
     }
 
 }
