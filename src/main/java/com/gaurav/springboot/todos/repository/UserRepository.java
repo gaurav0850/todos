37a2332505b8dc5f453ca22b14f4d1254c81bc5f
Gaurav <gauravwadibhasme0850@gmail.com>
2025-11-01 17:45:38 +0530

    countAdminUsers

--- src/main/java/com/gaurav/springboot/todos/repository/UserRepository.java
+++ src/main/java/com/gaurav/springboot/todos/repository/UserRepository.java
@@ -2,12 +2,13 @@ package com.gaurav.springboot.todos.repository;
 
 import com.gaurav.springboot.todos.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
-    //todo
+    @Query("SELECT COUNT(u) FROM User u JOIN u.authorities a WHERE a.authority = 'ROLE_ADMIN'")
     long countAdminUsers();
 }
