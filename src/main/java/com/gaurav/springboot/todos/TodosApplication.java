728781ec1b0657d6fc56deee1f7c598ffc2eb5ec
gaurav0850 <gauravwadibhasme0850@gmail.com>
2025-11-02 12:45:20 +0530

    time for project initialization

--- src/main/java/com/gaurav/springboot/todos/TodosApplication.java
+++ src/main/java/com/gaurav/springboot/todos/TodosApplication.java
@@ -2,12 +2,17 @@ package com.gaurav.springboot.todos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.StopWatch;
 
 @SpringBootApplication
 public class TodosApplication {
 
 	public static void main(String[] args) {
+        StopWatch stopWatch = new StopWatch("Application startup");
+        stopWatch.start();
 		SpringApplication.run(TodosApplication.class, args);
+        stopWatch.stop();
+        System.out.println(stopWatch.prettyPrint());
 	}
 
 }
