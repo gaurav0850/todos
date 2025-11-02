c46f0bceebc5256919fd2bb0e2a4c941912e431a
gaurav0850 <gauravwadibhasme0850@gmail.com>
2025-11-02 13:30:49 +0530

    updatePassword() method

--- src/main/java/com/gaurav/springboot/todos/service/UserService.java
+++ src/main/java/com/gaurav/springboot/todos/service/UserService.java
@@ -1,9 +1,11 @@
 package com.gaurav.springboot.todos.service;
 
+import com.gaurav.springboot.todos.request.PasswordUpdateRequest;
 import com.gaurav.springboot.todos.response.UserResponse;
 
 public interface UserService {
 
     UserResponse getUserInfo();
     void deleteUser();
+    void updatePassword(PasswordUpdateRequest passwordUpdateRequest);
 }
--- src/main/java/com/gaurav/springboot/todos/service/UserServiceImpl.java
+++ src/main/java/com/gaurav/springboot/todos/service/UserServiceImpl.java
@@ -3,9 +3,11 @@ package com.gaurav.springboot.todos.service;
 import com.gaurav.springboot.todos.entity.Authority;
 import com.gaurav.springboot.todos.entity.User;
 import com.gaurav.springboot.todos.repository.UserRepository;
+import com.gaurav.springboot.todos.request.PasswordUpdateRequest;
 import com.gaurav.springboot.todos.response.UserResponse;
 import com.gaurav.springboot.todos.util.FindAuthenticatedUser;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
@@ -15,10 +17,12 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final FindAuthenticatedUser findAuthenticatedUser;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository, FindAuthenticatedUser findAuthenticatedUser) {
+    public UserServiceImpl(UserRepository userRepository, FindAuthenticatedUser findAuthenticatedUser, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.findAuthenticatedUser = findAuthenticatedUser;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -46,6 +50,46 @@ public class UserServiceImpl implements UserService {
         }
 
         userRepository.delete(user);
+    }
+
+    @Override
+    @Transactional
+    public void updatePassword(PasswordUpdateRequest passwordUpdateRequest) {
+        User user = findAuthenticatedUser.getAuthenticatedUser();
+        // validate old password
+        String oldPassword = passwordUpdateRequest.getOldPassword();
+
+        if (!isOldPasswordCorrect(oldPassword, user.getPassword())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Current password is incorrect");
+        }
+
+        // validate new and confirmed password
+        String newPassword = passwordUpdateRequest.getNewPassword();
+        String newPassword2 = passwordUpdateRequest.getNewPassword2();
+
+        if (!isNewPasswordConfirmed(newPassword, newPassword2)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "New Passwords do not match");
+        }
+
+        //validate if new password and old password is different
+        if (!isNewAndOldPasswordDifferent(newPassword, oldPassword)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "New password must be different from old password");
+        }
+
+        user.setPassword(passwordEncoder.encode(newPassword));
+
+    }
+
+    private boolean isOldPasswordCorrect(String oldPassword, String currentPassword) {
+        return passwordEncoder.matches(oldPassword, currentPassword);
+    }
+
+    private boolean isNewPasswordConfirmed(String newPassword, String newPasswordConfirmation) {
+        return newPassword.equals(newPasswordConfirmation);
+    }
+
+    private boolean isNewAndOldPasswordDifferent(String newPassword, String oldPassword) {
+        return !newPassword.equals(oldPassword);
     }
 
     private boolean isLastAdmin(User user) {
