f1b416e55a3f200e6eb14ecaa2688c9fce1e7d01
gaurav0850 <gauravwadibhasme0850@gmail.com>
2025-11-02 13:47:59 +0530

    updatePassword() end point

--- src/main/java/com/gaurav/springboot/todos/controller/UserController.java
+++ src/main/java/com/gaurav/springboot/todos/controller/UserController.java
@@ -1,14 +1,13 @@
 package com.gaurav.springboot.todos.controller;
 
+import com.gaurav.springboot.todos.request.PasswordUpdateRequest;
 import com.gaurav.springboot.todos.response.UserResponse;
 import com.gaurav.springboot.todos.service.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
 
-@Tag(name="User REST API Endpoints", description = "Operations related to info about current user")
+@Tag(name = "User REST API Endpoints", description = "Operations related to info about current user")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -28,5 +27,10 @@ public class UserController {
     @DeleteMapping()
     public void deleteUser() {
         userService.deleteUser();
+    }
+
+    @PutMapping("/password")
+    public void updatePassword(@Valid @RequestBody PasswordUpdateRequest passwordUpdateRequest) throws Exception {
+        userService.updatePassword(passwordUpdateRequest);
     }
 }
