4a0aef56bc4b51da6e6cead7fd435bc3ec5e92bc
gaurav0850 <gauravwadibhasme0850@gmail.com>
2025-11-02 13:52:02 +0530

    Swagger Tags

--- src/main/java/com/gaurav/springboot/todos/controller/UserController.java
+++ src/main/java/com/gaurav/springboot/todos/controller/UserController.java
@@ -3,8 +3,10 @@ package com.gaurav.springboot.todos.controller;
 import com.gaurav.springboot.todos.request.PasswordUpdateRequest;
 import com.gaurav.springboot.todos.response.UserResponse;
 import com.gaurav.springboot.todos.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "User REST API Endpoints", description = "Operations related to info about current user")
@@ -18,17 +20,23 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "User information", description = "Get current user information")
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/info")
     public UserResponse getUserInfo() {
         UserResponse userInfo = userService.getUserInfo();
         return userInfo;
     }
 
+    @Operation(summary = "Delete User", description = "Delete current user account")
+    @ResponseStatus(HttpStatus.OK)
     @DeleteMapping()
     public void deleteUser() {
         userService.deleteUser();
     }
 
+    @Operation(summary = "Update Password", description = "Change user password after verification")
+    @ResponseStatus(HttpStatus.OK)
     @PutMapping("/password")
     public void updatePassword(@Valid @RequestBody PasswordUpdateRequest passwordUpdateRequest) throws Exception {
         userService.updatePassword(passwordUpdateRequest);
