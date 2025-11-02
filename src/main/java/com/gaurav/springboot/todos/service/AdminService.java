3462e9de41157403abd26eeeba88dc82c0b4d956
gaurav0850 <gauravwadibhasme0850@gmail.com>
2025-11-02 16:53:18 +0530

    promoteToAdmin()

--- src/main/java/com/gaurav/springboot/todos/service/AdminService.java
+++ src/main/java/com/gaurav/springboot/todos/service/AdminService.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface AdminService {
 
     List<UserResponse> getAllUsers();
+
+    UserResponse promoteToAdmin(long userId);
 }
--- src/main/java/com/gaurav/springboot/todos/service/AdminServiceImpl.java
+++ src/main/java/com/gaurav/springboot/todos/service/AdminServiceImpl.java
@@ -4,10 +4,14 @@ import com.gaurav.springboot.todos.entity.Authority;
 import com.gaurav.springboot.todos.entity.User;
 import com.gaurav.springboot.todos.repository.UserRepository;
 import com.gaurav.springboot.todos.response.UserResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AdminServiceImpl implements AdminService {
@@ -24,6 +28,26 @@ public class AdminServiceImpl implements AdminService {
         return userRepository.findAll().stream()
                 .map(this::convertUserToUserResponse)
                 .toList();
+    }
+
+    @Override
+    @Transactional
+    public UserResponse promoteToAdmin(long userId) {
+        Optional<User> userOptional = userRepository.findById(userId);
+
+        if (userOptional.isEmpty()
+                || userOptional.get().getAuthorities().stream().
+                anyMatch(authority -> "ROLE_ADMIN".equals(authority.getAuthority()))) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User does not exist or already an admin");
+        }
+        User user = userOptional.get();
+        List<Authority> authorities = new ArrayList<>();
+        authorities.add(new Authority("ROLE_EMPLOYEE"));
+        authorities.add(new Authority("ROLE_ADMIN"));
+
+        user.setAuthorities(authorities);
+
+        return convertUserToUserResponse(userRepository.save(user));
     }
 
     private UserResponse convertUserToUserResponse(User user) {
